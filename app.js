6f9d3f12888fc4de97323c004d05b86a5fc0da49
Laurel Verma <56593513+1laurelverma@users.noreply.github.com>
2024-01-28 12:40:49 +0530

    added cors in project

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const Doctors = require("./MOCK_DATA.json");
+var cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
+app.use(cors());
 dotenv.config();
 
 const hardcodedPort = 7000;
 const Port = process.env.PORT;
 
-console.log(Port);
+// console.log(Port);
 // Routes
 app.get("/api/doctors", (req, res) => {
   return res.json(Doctors);
@@ -15,9 +17,9 @@ app.get("/api/doctors", (req, res) => {
 
 app.get("/api/doctors/:city", (req, res) => {
   const city = req.params.city;
-  console.log("city : ", city);
+  // console.log("city : ", city);
   let doctorByCity = Doctors.filter((doctor) => doctor.city.toLowerCase() === city);
-  console.log("doctorByCity", doctorByCity);
+  // console.log("doctorByCity", doctorByCity);
   return res.json(doctorByCity);
 });
 
